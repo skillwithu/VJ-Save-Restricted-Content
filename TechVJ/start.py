c9455f3b06787dab98a0fce7472e9d42df26d01c
skillwithu <skillwithu@gmail.com>
2025-07-13 08:02:27 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @staystrongbros
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@staystrongbros
+# Ask Doubt on telegram @staystrongbros
 
 import os
 import asyncio 
